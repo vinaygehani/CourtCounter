f6b53e1a868c47243e728468abd3c4315dad65d4
vinaygehani <icehammer@gmail.com>
2016-04-29 16:31:41 +0530

    a

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int scoreCounterA=0;
-    public int scoreCounterB=0;
+    public int scoreCounterA = 0;
+    public int scoreCounterB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayScore();
+        displayDefaultScore();
     }
 
 
@@ -25,27 +25,56 @@ public class MainActivity extends AppCompatActivity {
             case (R.id.a1):
                 scoreCounterA++;
                 displayScore(R.id.team_a_score, scoreCounterA);
+                break;
 
-
+            case (R.id.a2):
+                scoreCounterA = scoreCounterA + 2;
+                displayScore(R.id.team_a_score, scoreCounterA);
+                break;
+            case (R.id.a3):
+                scoreCounterA = scoreCounterA + 3;
+                displayScore(R.id.team_a_score, scoreCounterA);
+                break;
+            case (R.id.b1):
+                scoreCounterB++;
+                displayScore(R.id.team_b_score, scoreCounterB);
+                break;
+            case (R.id.b2):
+                scoreCounterB = scoreCounterB + 2;
+                displayScore(R.id.team_b_score, scoreCounterB);
+                break;
+            case (R.id.b3):
+                scoreCounterB = scoreCounterB + 3;
+                displayScore(R.id.team_b_score, scoreCounterB);
+                break;
         }
     }
 
-    public void displayScore(int v, int score){
+    public void displayScore(int v, int score) {
 
         TextView scoreView = (TextView) findViewById(v);
-        scoreView.setText(score);
+        scoreView.setText(String.valueOf(score));
 
 
     }
 
-    public void displayScore(){
+    public void displayDefaultScore(View v) {
+
+
+        scoreCounterA = 0;
+        scoreCounterB = 0;
+        displayDefaultScore();
+
+    }
+
+    public void displayDefaultScore() {
+
 
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(0);
+        scoreView.setText(String.valueOf(0));
 
         scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(0);
-
+        scoreView.setText(String.valueOf(0));
 
     }
 }
